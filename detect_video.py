8ea21c3602210e936ce97b50c8280e39cd58e979
Noba1anc3 <zxryhjp@yahoo.co.jp>
2020-05-15 11:51:54 +0800

    detect_video

--- detect_video.py
+++ detect_video.py
@@ -1,21 +1,23 @@
+import cv2
 import time
+
 from absl import app, flags, logging
 from absl.flags import FLAGS
-import cv2
+
 import tensorflow as tf
+
 from yolov3_tf2.models import (
     YoloV3, YoloV3Tiny
 )
 from yolov3_tf2.dataset import transform_images
 from yolov3_tf2.utils import draw_outputs
 
-
 flags.DEFINE_string('classes', './data/coco.names', 'path to classes file')
 flags.DEFINE_string('weights', './checkpoints/yolov3.tf',
                     'path to weights file')
 flags.DEFINE_boolean('tiny', False, 'yolov3 or yolov3-tiny')
 flags.DEFINE_integer('size', 416, 'resize images to')
-flags.DEFINE_string('video', './1.mp4',
+flags.DEFINE_string('video', 'http://admin:admin@192.168.1.105:8081/',
                     'path to video file or number for webcam)')
 flags.DEFINE_string('output', None, 'path to output video')
 flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when saving video to file')
@@ -70,11 +72,11 @@ def main(_argv):
         t1 = time.time()
         boxes, scores, classes, nums = yolo.predict(img_in)
         t2 = time.time()
-        times.append(t2-t1)
+        times.append(t2 - t1)
         times = times[-20:]
 
         img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
-        img = cv2.putText(img, "Time: {:.2f}ms".format(sum(times)/len(times)*1000), (0, 30),
+        img = cv2.putText(img, "Time: {:.2f}ms".format(sum(times) / len(times) * 1000), (0, 30),
                           cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
         if FLAGS.output:
             out.write(img)
